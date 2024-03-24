632a839b43bd7f2557fce2cbfdc1bf78d74db2ad
johnsonwlu <lujohnson2000@gmail.com>
2024-03-23 21:02:53 -0700

    style change

--- notes-page-application-frontend/src/index.js
+++ notes-page-application-frontend/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import { store } from './reducers/store';
 
-// const store = configureStore({reducer: rootReducer});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
